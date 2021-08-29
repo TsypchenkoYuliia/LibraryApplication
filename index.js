e6dd8047fb149ac8e57c9743009b75bb49134a92
TsypchenkoYuliia <57401112+TsypchenkoYuliia@users.noreply.github.com>
2021-08-29 20:54:37 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+// (function(angular) {
+//     'use strict';
+//   angular.module('libraryApp', []);
+//   })(window.angular);
+
+  angular.module('libraryApp', []);
+
+//   (function(angular) {
+//     'use strict';
+//   angular.module('libraryApp')
+//   .config(['$routeProvider',
+//   function config($routeProvider) {
+//     $routeProvider.
+//       when('/books', {
+//         template: '<book-list></book-list>'
+//       }).
+//       when('/books/:bookId', {
+//         template: '<book-detail></book-detail>'
+//       }).
+//       otherwise('/books');
+//   }
+// ])})(window.angular);
